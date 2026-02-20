7c96c875c6a6047fdf408d0c37f551c4e1227bd3
galapsegales <galapsegales@gmail.com>
2026-02-20 13:40:18 +0100

    problemas deployment en gh pages solucion

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/academy-demo/', 
+  base: '/academy-demo/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        quienessomos: resolve(__dirname, 'quienessomos.html'),
+      },
+    },
+  },
 })
 
